cad4a4c703e6af835bb77a4fb90dbfe482047aff
Nick Shoup <shoupnb@gmail.com>
2020-01-05 19:47:13 -0700

    added the membership command class and some stuff to the sql scripts

---
+++ membership/Commands/Membership/AuthenticateCommand.cs
@@ -0,0 +1,42 @@
+﻿using Membership.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Membership.Commands.Membership
+{
+    public class AuthenticationResult
+    {
+        public long UserID { get; set; }
+        public long SessionID { get; set; }
+        public string Message { get; set; }
+        public bool Success { get; set; }
+        public string DisplayName { get; set; }
+    }
+
+    public class AuthenticateCommand
+    {
+        ICommandRunner Runner;
+        public AuthenticateCommand(ICommandRunner runner)
+        {
+            Runner = runner;
+            this.ProviderName = "local";
+        }
+
+        public string ProviderName { get; set; }
+        public string ProviderKey { get; set; }
+        public string ProviderValue { get; set; }
+
+        public AuthenticationResult Execute()
+        {
+            var sql = "select * from authenticate(@0,@1,@2)";
+            return Runner.ExecuteSingle<AuthenticationResult>(sql,
+              this.ProviderKey,
+              this.ProviderValue,
+              this.ProviderName);
+        }
+
+    }
+}
---
+++ membership/Commands/Membership/RegisterCommand.cs
@@ -0,0 +1,46 @@
+﻿using Membership.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Membership.Commands.Membership
+{
+    public class RegistrationResult
+    {
+        public long NewUserId { get; set; }
+        public string Email { get; set; }
+        public string  Status { get; set; }
+        public bool Success { get; set; }
+        public string Message { get; set; }
+        public string Token { get; set; }
+    }
+    public class RegisterCommand
+    {
+
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Confirmation { get; set; }
+        public string IP { get; set; }
+        public string First { get; set; }
+        public string Last { get; set; }
+        public string Status { get; set; }
+
+        ICommandRunner CommandRunner;
+        public RegisterCommand(ICommandRunner commandRunner)
+        {
+            this.CommandRunner = commandRunner;
+            Status = "pending";
+            IP = "127.0.0.1";
+        }
+
+        public RegistrationResult Execute()
+        {
+            var sql = $"select * from register(@0, @1, @2, @3, @4, @5, @6);";
+            return CommandRunner.ExecuteSingle<RegistrationResult>(sql, 
+                this.Email, this.Password, this.Confirmation, 
+                this.IP, this.Status, this.First, this.Last);
+        }
+    }
+}
